197daf2a7a3b9ff5a51bdf7759ed4a7f4166463c
Christovis <chrbecker01@gmail.com>
2021-02-12 13:18:19 +0000

    (test_)listserv.py: made self-contained class that can scrape the web and read from local memory

--- tests/test_listserv.py
+++ tests/test_listserv.py
@@ -1,137 +1,108 @@
-import os
-from pathlib import Path
-
-import numpy.testing as npt
 import pytest
 
-from bigbang import listserv
+from bigbang.listserv import ListservArchive, ListservList, ListservMessage
 
-dir_test_base = Path(__file__).parent.absolute()
-dir_path_data = str(dir_test_base / "data/3GPP/") + "/"
-file_id = "3GPP_TSG_SA_WG2_UPCON.LOG*"
-
-
-@pytest.fixture(name="file_paths", scope="module")
-def test__get_all_file():
-    file_paths = listserv._get_all_file(dir_path_data, file_id)
-    assert len(file_paths) == 2
-    return file_paths
+url_archive = "https://list.etsi.org/scripts/wa.exe?"
+url_list = url_archive + "A0=3GPP_TSG_CT_WG6"
+url_message = url_archive + "A2=ind2101A&L=3GPP_TSG_CT_WG6&O=D&P=1870"
 
 
-@pytest.fixture(name="temp_file_path", scope="module")
-def test__create_temporary_file():
-    temp_file_path = listserv._create_temporary_file()
-    assert os.path.isfile(temp_file_path)
-    return temp_file_path
+class TestListservMessage:
+    @pytest.fixture(name="msg", scope="module")
+    def test__from_url(self):
+        msg = ListservMessage.from_url(
+            list_name="3GPP_TSG_CT_WG6",
+            url=url_message,
+            fields="total",
+        )
+        assert msg.body.split(",")[0] == "Dear 3GPP CT people"
+        assert msg.subject == "Happy New Year 2021"
+        assert msg.fromname == "Kimmo Kymalainen"
+        assert msg.fromaddr == "[log in to unmask]"
+        assert msg.toname == "Kimmo Kymalainen"
+        assert msg.toaddr == "[log in to unmask]"
+        assert msg.date == "Tue Jan  5 12:15:30 2021"
+        assert msg.contenttype == "multipart/related"
+        return msg
+
+    def test__only_header_from_url(self):
+        msg = ListservMessage.from_url(
+            list_name="3GPP_TSG_CT_WG6",
+            url=url_message,
+            fields="header",
+        )
+        assert msg.body is None
+
+    def test__only_body_from_url(self):
+        msg = ListservMessage.from_url(
+            list_name="3GPP_TSG_CT_WG6",
+            url=url_message,
+            fields="body",
+        )
+        assert msg.subject is None
+
+    def test__to_dict(self, msg):
+        dic = msg.to_dict()
+        assert len(list(dic.keys())) == 8
 
 
-@pytest.fixture(name="content", scope="module")
-def read_listserv_file_content(file_paths):
-    file_path = file_paths[0]  # select one file path from list
-    file = open(file_path, "r")
-    content = file.readlines()
-    return content
+class TestListservList:
+    @pytest.fixture(name="mlist", scope="module")
+    def test__from_url(self):
+        mlist = ListservList.from_url(
+            name="3GPP_TSG_CT_WG6",
+            url=url_list,
+            select={
+                "years": 2021,
+                "months": "January",
+                "weeks": 1,
+                "fields": "header",
+            },
+        )
+        assert mlist.name == "3GPP_TSG_CT_WG6"
+        assert mlist.source == url_list
+        assert len(mlist) == 3
+        assert mlist.messages[0].subject == "Happy New Year 2021"
+        return mlist
+
+    def test__to_dict(self, mlist):
+        dic = mlist.to_dict()
+        assert len(list(dic.keys())) == 8
+        assert len(dic[list(dic.keys())[0]]) == 3
+
+    def test__to_pandas_dataframe(self, mlist):
+        df = mlist.to_pandas_dataframe()
+        assert len(df.columns.values) == 8
+        assert len(df.index.values) == 3
 
 
-@pytest.fixture(name="line_nrs_header_start", scope="module")
-def test__get_line_numbers_of_header_starts(content):
-    lnrs_header_start_received = listserv._get_line_numbers_of_header_starts(
-        content
-    )
-    lnrs_header_start_expected = [0, 265, 496, -1]
-    assert all(
-        [
-            a == b
-            for a, b in zip(
-                lnrs_header_start_received, lnrs_header_start_expected
-            )
-        ]
-    )
-    return lnrs_header_start_received
+class TestListservArchive:
+    @pytest.fixture(name="arch", scope="session")
+    def test__from_url(self):
+        arch = ListservArchive.from_url(
+            name="3GPP",
+            url_root=url_archive,
+            url_home=url_archive + "HOME",
+            select={
+                "years": 2021,
+                "months": "January",
+                "weeks": 1,
+                "fields": "header",
+            },
+        )
+        assert arch.name == "3GPP"
+        assert arch.url == url_archive
+        assert len(arch) == 4
+        assert len(arch.lists[0]) == 3
+        assert arch.lists[0].messages[0].subject == "Happy New Year 2021"
+        return arch
 
+    def test__to_dict(self, arch):
+        dic = arch.to_dict()
+        assert len(list(dic.keys())) == 9
+        assert len(dic[list(dic.keys())[0]]) == 40
 
-@pytest.fixture(name="line_nrs_header_end", scope="module")
-def test__get_line_numbers_of_header_ends(content, line_nrs_header_start):
-    lnrs_header_end_received = listserv._get_line_numbers_of_header_ends(
-        content, line_nrs_header_start
-    )
-    lnrs_header_end_expected = [30, 297, 506]
-    assert all(
-        [
-            a == b
-            for a, b in zip(lnrs_header_end_received, lnrs_header_end_expected)
-        ]
-    )
-    return lnrs_header_end_received
-
-
-def test__get_header(file_paths, line_nrs_header_start, line_nrs_header_end):
-    file_path = file_paths[0]  # select one file path from list
-    file = open(file_path, "r")
-    content = file.readlines()
-    header_dic = listserv._get_header(
-        content[line_nrs_header_start[0] : line_nrs_header_end[0] + 1]
-    )
-    header_keys_received = list(header_dic.keys())
-    header_keys_expected = [
-        "Date",
-        "Reply-To",
-        "From",
-        "Subject",
-        "Comments",
-        "Content-Type",
-        "MIME-Version",
-        "Message-ID",
-        "Mm-Date",
-        "Mm-From",
-        "Mm-Name",
-        "In-Reply-To",
-    ]
-    assert all(
-        [a == b for a, b in zip(header_keys_received, header_keys_expected)]
-    )
-
-
-def test__convert_to_mailman(file_paths, temp_file_path):
-    file_path = file_paths[0]  # select one file path from list
-    listserv._convert_to_mailman(file_path, temp_file_path)
-    file = open(temp_file_path, "r")
-    content = file.readlines()
-    print(content[3])
-    assert (
-        content[2]
-        == "From b'Andreas.Maeder@NECLAB.EU' Thu Aug  1 10:46:56 2013\n"
-    )
-    assert content[4] == "MIME-Version: 1.0 \n"
-
-
-def test__from_file(file_paths):
-    file_path = file_paths[0]  # select one file path from list
-    df = listserv.from_file(file_path)
-    assert len(df.index.values) == 3
-    columns_received = list(df.columns.values)
-    columns_expected = [
-        "From",
-        "Subject",
-        "Date",
-        "In-Reply-To",
-        "References",
-        "Body",
-    ]
-    assert all([a == b for a, b in zip(columns_received, columns_expected)])
-
-
-def test__from_files():
-    df = listserv.from_files([dir_path_data], [file_id])
-    print(len(df.index.values))
-    assert len(df.index.values) == 9
-    columns_received = list(df.columns.values)
-    columns_expected = [
-        "From",
-        "Subject",
-        "Date",
-        "In-Reply-To",
-        "References",
-        "Body",
-    ]
-    assert all([a == b for a, b in zip(columns_received, columns_expected)])
+    def test__to_pandas_dataframe(self, arch):
+        df = arch.to_pandas_dataframe()
+        assert len(df.columns.values) == 9
+        assert len(df.index.values) == 40
